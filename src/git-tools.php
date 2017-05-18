a49b39bc46e6b27d567b270deda067c1b280d55d
Nikita Loges <git@nikita-loges>
2017-05-18 16:55:26 +0200

    testing

--- src/git-tools.php
+++ src/git-tools.php
@@ -1,6 +1,6 @@
 #!/usr/bin/env php
 <?php
-require __DIR__ . '/..//vendor/autoload.php';
+require __DIR__ . '/../vendor/autoload.php';
 
 use Symfony\Component\Console\Application;
 use Tenolo\GitTools\Command as GitCommand;
